2ce55077c3d247038e6ea9b61d5a54f8d5e5fba4
chemokita13 <chemokita13@gmail.com>
2023-05-07 20:47:40 +0200

    /ping route added to check server status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { HttpStatus } from '@nestjs/common/enums';
 
+@ApiTags('Server')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Ping the server' })
+  @ApiResponse({
+    description: `API is down or not responding.`,
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+  })
+  @ApiResponse({
+    description: `API is up and responding.`,
+    status: HttpStatus.OK,
+  })
   @Get('/ping')
   Ping(): string {
     return this.appService.ping();
