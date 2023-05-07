43ff642e7583e6506302367142d95a243ebf718e
chemokita13 <chemokita13@gmail.com>
2023-05-07 20:52:35 +0200

    Improved /ping route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,13 +12,33 @@ export class AppController {
   @ApiResponse({
     description: `API is down or not responding.`,
     status: HttpStatus.INTERNAL_SERVER_ERROR,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            statusCode: 500,
+            message: 'Internal Server Error',
+          },
+        },
+      },
+    },
   })
   @ApiResponse({
     description: `API is up and responding.`,
     status: HttpStatus.OK,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            statusCode: 200,
+            message: 'Pong!',
+          },
+        },
+      },
+    },
   })
   @Get('/ping')
-  Ping(): string {
+  Ping(): { statusCode: number; message: string } {
     return this.appService.ping();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  ping(): string {
-    return 'Pong!';
+  ping(): { statusCode: number; message: string } {
+    return { statusCode: 200, message: 'Pong' };
   }
 }
