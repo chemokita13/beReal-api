ff49aa69b58c2e6dc37245136a248f43f2cb563c
chemokita13 <chemokita13@gmail.com>
2023-05-09 20:45:22 +0200

    login routes finished

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,18 +1,143 @@
 import { Controller } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { Post, Body } from '@nestjs/common';
+import { APIresponse } from 'src/types/types';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Login')
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
+  @ApiParam({
+    name: 'phone',
+    description: 'Phone number to send otp code',
+    type: 'string',
+    example: '+34123456789',
+  })
+  @ApiOperation({ summary: 'Send otp code to your phone' })
+  @ApiResponse({
+    description: `OTP sent.`,
+    status: 200,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            status: 200,
+            message: 'OTP sent',
+            data: {
+              otpSesion: {
+                otpSesion: 'exampleexampleexampleexampleexampleexample',
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    description: `OTP not sent.`,
+    status: 400,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            status: 400,
+            message: 'OTP not sent',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    description: `Internal server error.`,
+    status: 500,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            status: 500,
+            message: 'Internal server error',
+            data: 'Any error',
+          },
+        },
+      },
+    },
+  })
   @Post('/send-code')
-  SendCode(@Body() body: { phone: string }): Promise<any> {
+  SendCode(@Body() body: { phone: string }): Promise<APIresponse> {
     return this.loginService.sendCode(body);
   }
 
+  @ApiParam({
+    name: 'code',
+    description: 'Code to verify',
+    type: 'string',
+    example: '123456',
+  })
+  @ApiParam({
+    name: 'otpSesion',
+    description: 'Otp session returned in send-code endpoint',
+    type: 'string',
+    example: 'exampleexampleexampleexample',
+  })
+  @ApiOperation({ summary: 'Verify otp code' })
+  @ApiResponse({
+    description: `OTP verified.`,
+    status: 200,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            access: {
+              refresh_token: 'example',
+              token: 'exampleexampleexampleexampleexampleexample',
+              expires: '2023-05-07T00:57:49+02:00',
+            },
+            firebase: {
+              refresh_token: 'exampleexampleexampleexample',
+              token: 'exampleexampleexampleexampleexample',
+              expires: '2023-05-07T01:06:37+02:00',
+            },
+            userId: 'exampleexampleexampleexample',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    description: `OTP not verified.`,
+    status: 400,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            status: 400,
+            message: 'OTP not verified',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    description: `Internal server error.`,
+    status: 500,
+    content: {
+      'application/json': {
+        schema: {
+          example: {
+            status: 500,
+            message: 'Internal server error',
+            data: 'Any error',
+          },
+        },
+      },
+    },
+  })
   @Post('/verify')
-  VerifyCode(@Body() body: { phone: string; code: string }): Promise<any> {
+  VerifyCode(
+    @Body() body: { code: string; otpSesion: string },
+  ): Promise<APIresponse> {
     return this.loginService.verifyCode(body);
   }
 }
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,11 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import BeFake from 'src/BeFake/BeFake';
+import { BeFakeResponse } from 'src/BeFake/types/BeFakeResponse';
+import { APIresponse } from 'src/types/types';
 
 @Injectable()
 export class LoginService {
-  sendCode(body: { phone: string }): Promise<any> {
-    throw new Error('Method not implemented.');
+  async sendCode(body: { phone: string }): Promise<APIresponse> {
+    try {
+      const bf = new BeFake();
+      const response: BeFakeResponse = await bf.sendOtpVonage(body.phone);
+      if (response.done) {
+        return {
+          status: 200,
+          message: 'OTP sent',
+          data: {
+            otpSesion: response.data,
+          },
+        };
+      }
+      return {
+        status: 400,
+        message: 'OTP not sent',
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        message: 'Internal server error',
+        data: error,
+      };
+    }
   }
-  verifyCode(body: { phone: string; code: string }): Promise<any> {
-    throw new Error('Method not implemented.');
+
+  async verifyCode(body: {
+    code: string;
+    otpSesion: string;
+  }): Promise<APIresponse> {
+    try {
+      const bf = new BeFake();
+      const response: BeFakeResponse = await bf.verifyOtpVonage(
+        body.code,
+        body.otpSesion,
+      );
+      if ((response.done = true)) {
+        return {
+          status: 200,
+          message: 'OTP verified',
+          data: {
+            tokenObj: bf.saveToken(),
+          },
+        };
+      }
+      return {
+        status: 400,
+        message: 'OTP not verified',
+      };
+    } catch (error) {
+      return {
+        status: 500,
+        message: 'Internal server error',
+        data: error,
+      };
+    }
   }
 }
