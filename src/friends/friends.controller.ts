a42221a062e1a9f6caf7b7351f4f51238b1f4f13
chemokita13 <chemokita13@gmail.com>
2023-05-13 15:06:03 +0200

    get friends info route finished

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -64,4 +64,61 @@ export class FriendsController {
         const token = req.headers.token;
         return this.friendsService.getFeed(token);
     }
+
+    @ApiHeader({
+        name: 'token',
+        description: 'JWT Token returned in /login/verify route',
+        required: true,
+    })
+    @ApiOperation({ summary: 'Get your friends info list' })
+    @ApiResponse({
+        description: `Friends generated.`,
+        status: 200,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 200,
+                        message: 'Friends generated',
+                        data: 'LARGE JSON WITH FRIENDS DATA',
+                    },
+                },
+            },
+        },
+    })
+    @ApiResponse({
+        description: `Token not generated.`,
+        status: 400,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 400,
+                        message: 'Token not generated',
+                        data: 'LARGE JSON WITH ERROR DATA',
+                    },
+                },
+            },
+        },
+    })
+    @ApiResponse({
+        description: `Internal server error.`,
+        status: 500,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 500,
+                        message: 'Internal server error',
+                        data: 'LARGE JSON WITH ERROR DATA',
+                    },
+                },
+            },
+        },
+    })
+    @Get('/info')
+    GetInfo(@Req() req: any): Promise<APIresponse> {
+        const token = req.headers.token;
+        return this.friendsService.getFriends(token);
+    }
 }
--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -6,7 +6,7 @@ import { APIresponse } from 'src/types/types';
 @Injectable()
 export class FriendsService {
     constructor(private readonly loginService: LoginService) {}
-    async getFeed(token: string) {
+    async getFeed(token: string): Promise<APIresponse> {
         try {
             const { status, data }: APIresponse =
                 await this.loginService.getToken(token);
@@ -23,6 +23,33 @@ export class FriendsService {
                 status: 200,
                 message: 'Feed generated',
                 data: feed,
+            };
+        } catch (error) {
+            return {
+                status: 500,
+                message: 'Internal server error',
+                data: error,
+            };
+        }
+    }
+
+    async getFriends(token: string): Promise<APIresponse> {
+        try {
+            const { status, data }: APIresponse =
+                await this.loginService.getToken(token);
+            if (status != 200) {
+                return {
+                    status: 400,
+                    message: 'Token not generated',
+                    data: data,
+                };
+            }
+            const bf: BeFake = new BeFake(data);
+            const friends = await bf.getFriends();
+            return {
+                status: 200,
+                message: 'Friends generated',
+                data: friends,
             };
         } catch (error) {
             return {
