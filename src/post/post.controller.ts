4b0d4a5218e26ba7b01a9baa42c98787de8f7e6c
chemokita13 <chemokita13@gmail.com>
2023-05-14 17:21:23 +0200

    post a photo finished

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,135 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Post,
+    Req,
+    UploadedFiles,
+    UseInterceptors,
+} from '@nestjs/common';
 import { PostService } from './post.service';
+import { APIresponse, PostData } from 'src/types/types';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import {
+    ApiBody,
+    ApiExtraModels,
+    ApiHeader,
+    ApiOperation,
+    ApiParam,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Post')
 @Controller('post')
 export class PostController {
     constructor(private readonly postService: PostService) {}
+    @ApiExtraModels(PostData)
+    @ApiParam({
+        name: 'img1',
+        description: 'Image 1',
+        type: 'file',
+        required: true,
+    })
+    @ApiParam({
+        name: 'img2',
+        description: 'Image 2',
+        type: 'file',
+        required: true,
+    })
+    @ApiParam({
+        name: 'post data',
+        description: 'Post data',
+        type: 'object',
+        required: true,
+        schema: {
+            type: 'PostData see it scrolling down',
+        },
+    })
+    @ApiOperation({ summary: 'Create a new post' })
+    @ApiResponse({
+        description: `Post created.`,
+        status: 200,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 200,
+                        message: 'Post created',
+                        data: 'LARGE JSON WITH POST DATA',
+                    },
+                },
+            },
+        },
+    })
+    @ApiResponse({
+        description: `Token not generated.`,
+        status: 400,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 400,
+                        message: 'Token not generated',
+                        data: 'LARGE JSON WITH ERROR DATA',
+                    },
+                },
+            },
+        },
+    })
+    @ApiResponse({
+        description: `Internal server error.`,
+        status: 500,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 500,
+                        message: 'Internal server error',
+                        data: 'LARGE JSON WITH ERROR DATA',
+                    },
+                },
+            },
+        },
+    })
+    @ApiHeader({
+        name: 'token',
+        description: 'JWT Token returned in /login/verify route',
+        required: true,
+    })
+    @ApiResponse({
+        description: `Internal server error.`,
+        status: 500,
+        content: {
+            'application/json': {
+                schema: {
+                    example: {
+                        status: 500,
+                        message: 'Internal server error',
+                        data: 'LARGE JSON WITH ERROR DATA',
+                    },
+                },
+            },
+        },
+    })
+    @Post('/new')
+    @UseInterceptors(
+        FileFieldsInterceptor([
+            { name: 'img1', maxCount: 1 },
+            { name: 'img2', maxCount: 1 },
+        ]),
+    )
+    createPost(
+        @Body('postData') postData: string,
+        @Req() req: any,
+        @UploadedFiles()
+        files: { img1?: Express.Multer.File[]; img2?: Express.Multer.File[] },
+    ): Promise<APIresponse> {
+        const token = req.headers.token;
+        return this.postService.createPost(
+            token,
+            postData,
+            files.img1[0].buffer,
+            files.img2[0].buffer,
+        );
+    }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import BeFake from 'src/BeFake/BeFake';
+import { BeFakeResponse } from 'src/BeFake/types/BeFakeResponse';
+import { LoginService } from 'src/login/login.service';
+import { APIresponse, PostData } from 'src/types/types';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(private readonly loginService: LoginService) {}
+    // Gets a string and return an Uint8Array
+    private JsonStringToUint8Array(jsonString: string): Uint8Array {
+        var uint8array = new Uint8Array(jsonString.length);
+        for (var i = 0; i < jsonString.length; i++) {
+            uint8array[i] = jsonString.charCodeAt(i);
+        }
+        return uint8array;
+    }
+    async createPost(
+        token: string,
+        postData: string, // string<json:PostData>
+        img1: Buffer,
+        img2: Buffer,
+    ): Promise<APIresponse> {
+        try {
+            // Convert string to PostData Object(json)
+            const postObj: PostData = JSON.parse(postData);
+            // Get tokens data and status from Object
+            const { status, data }: APIresponse =
+                await this.loginService.getToken(token);
+            if (status != 200) {
+                return {
+                    status: 400,
+                    message: 'Token not generated',
+                    data: data,
+                };
+            }
+            const bf: BeFake = new BeFake(data);
+            const post: BeFakeResponse = await bf.postUpload(
+                img1,
+                img2,
+                postObj.resize,
+                postObj.late,
+                postObj.visibility,
+                postObj.retakes,
+                postObj.caption,
+                postObj.taken_at,
+                postObj.location,
+            );
+            if (!post.done) {
+                return {
+                    status: 400,
+                    message: 'Post not created',
+                    data: post,
+                };
+            }
+            return {
+                status: 200,
+                message: 'Post created',
+                data: post.data.data,
+            };
+        } catch (error) {
+            return {
+                status: 500,
+                message: 'Internal Server Error',
+                data: error,
+            };
+        }
+    }
+}
