742efd3c9323da0053e0956aa58c52554909bfc1
chemokita13 <chemokita13@gmail.com>
2023-05-14 14:15:09 +0200

    max request limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ limit: '50mb', extended: true }));
     const config = new DocumentBuilder()
         .setTitle('BeReal API')
         .setDescription('An unofficial API for BeReal')
