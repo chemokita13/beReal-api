724141f50a3803fc5d5f13a39ecaf095bc901d80
chemokita13 <chemokita13@gmail.com>
2023-05-14 15:35:44 +0200

    imported login module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { LoginModule } from 'src/login/login.module';
 
 @Module({
     controllers: [PostController],
     providers: [PostService],
+    imports: [LoginModule],
 })
 export class PostModule {}
