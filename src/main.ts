ec73437e9833619e42e4ab57bd7bf93cac2c1f79
chemokita13 <chemokita13@gmail.com>
2023-05-26 22:36:59 +0200

    env.PORT added

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
         origin: '*',
         credentials: true,
     });
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
