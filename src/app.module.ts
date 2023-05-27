63a0a5a42b03736ed90907185980d030ed154fa1
chemokita13 <chemokita13@gmail.com>
2023-05-27 14:06:28 +0200

    clean code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 import { PostModule } from './post/post.module';
-import { FriendsModule } from './friends/friends.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [LoginModule, PostModule, FriendsModule],
+    imports: [LoginModule, PostModule, ConfigModule.forRoot()],
     controllers: [AppController],
     providers: [AppService],
 })
