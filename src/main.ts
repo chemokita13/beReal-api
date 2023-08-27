d5f0d83a6d7c35f68bd49cedcf8a145c94c9d9fe
José María Pahino <chemokita13@gmail.com>
2023-08-27 23:40:03 +0200

    bytes limit request

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ limit: '50mb', extended: true }));
+    app.use(json({ limit: '100mb' }));
+    app.use(urlencoded({ limit: '100mb', extended: true }));
     const config = new DocumentBuilder()
         .setTitle('BeReal API')
         .setDescription('An unofficial API for BeReal')
