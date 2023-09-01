f11b6488c34d879d2f6e13295c1d4538ea6f3ec8
José María Pahino <chemokita13@gmail.com>
2023-09-01 16:28:40 +0200

    port loggin error solved

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
         credentials: true,
     });
     await app.listen(process.env.PORT || 3000, () =>
-        console.log('Listening on port ' + process.env.PORT || 3000),
+        console.log('Listening on port ' + (process.env.PORT || 3000)),
     );
 }
 bootstrap();
